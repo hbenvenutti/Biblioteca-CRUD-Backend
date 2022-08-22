9a47793bd6f9852ba461e276f34cefe84c197294
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-22 19:31:52 -0300

    feat(accounts): criado o repositório de users e a sua interface

---
+++ src/modules/accounts/infra/firestore/repositories/Users.repository.ts
@@ -0,0 +1,31 @@
+import { CreateUserDTO } from '@accounts:dtos/CreateUser.dto';
+import { User } from '@accounts:entities/User';
+import { UsersRepositoryInterface } from '@accounts:repositories-interfaces/UsersRepository.interface';
+
+// ---------------------------------------------------------------------------------------------- //
+
+class UsersRepository implements UsersRepositoryInterface {
+  create(data: CreateUserDTO): Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+
+  list(): Promise<User[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  findByEmail(email: string): Promise<User> | undefined {
+    throw new Error('Method not implemented.');
+  }
+
+  findById(id: string): Promise<User[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+export { UsersRepository };
---
+++ src/modules/accounts/repositories/UsersRepository.interface.ts
@@ -0,0 +1,10 @@
+import { CreateUserDTO } from '@accounts/dtos/CreateUser.dto';
+import { User } from '@accounts:entities/User';
+
+export interface UsersRepositoryInterface {
+  create(data: CreateUserDTO): Promise<User>;
+  list(): Promise<User[]>;
+  findByEmail(email: string): Promise<User> | undefined;
+  findById(id: string): Promise<User[]>;
+  delete(id: string): Promise<void>;
+}

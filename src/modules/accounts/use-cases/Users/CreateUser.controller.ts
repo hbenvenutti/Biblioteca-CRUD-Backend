e3c6483f2c3eb881fd09ecf11ea5aed5429acabb
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-22 21:51:37 -0300

    feat(accounts): implementado o controller de criação de usuários

--- src/modules/accounts/use-cases/Users/CreateUser.controller.ts
+++ src/modules/accounts/use-cases/Users/CreateUser.controller.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { CreateUserService } from '@accounts:use-cases/Users/CreateUser.service';
+
+// ---------------------------------------------------------------------------------------------- //
 
 export class CreateUserController {
   async execute(request: Request, response: Response): Promise<Response> {
     const { name, lastName, email, password, passwordConfirmation } = request.body;
-    
-    return response.status(200).json({});
+
+    const createUserService = container.resolve(CreateUserService);
+
+    const user = await createUserService.execute({
+      email,
+      name,
+      lastName,
+      password,
+      passwordConfirmation
+    });
+
+    return response.status(200).json(user);
   }
 }

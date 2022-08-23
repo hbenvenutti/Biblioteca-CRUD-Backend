2bdad23bae28797034997136c928a622f59ea1ca
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-22 21:58:01 -0300

    feat(accounts): criada a rota de criação de usuário no express

--- src/modules/accounts/infra/http/users.routes.ts
+++ src/modules/accounts/infra/http/users.routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import { CreateUserController } from '@accounts:use-cases/users/CreateUser.controller';
+
+// ---------------------------------------------------------------------------------------------- //
+
 const usersRouter = Router();
+
+const createUserController = new CreateUserController();
+
+usersRouter.get('/', createUserController.execute);
+
+// ---------------------------------------------------------------------------------------------- //
 
 export { usersRouter };

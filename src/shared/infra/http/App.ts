18671a0507d3e24e11716d97e49abe90fd05bb01
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-22 22:19:21 -0300

    feat(express): implementado o express async errors

--- src/shared/infra/http/App.ts
+++ src/shared/infra/http/App.ts
@@ -2,8 +2,10 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import '@shared:containers/index';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
+import { AppError } from '@errors/App.error';
 import { routes } from '@shared:routes/index.routes';
 
 // ---------------------------------------------------------------------------------------------- //
@@ -13,6 +15,7 @@ class App {
   constructor() {
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -21,6 +24,24 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    this.server.use((err: Error, request: Request, response: Response, _:NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({ status: 'error', message: err.message });
+      }
+
+      if (process.env.NODE_ENV !== 'production') {
+        return response.status(500).json({ status: 'error', message: err.message });
+      }
+
+      return response.status(500).json({
+        status: 'error',
+        message: 'internal server error'
+      });
+    });
   }
 }
 

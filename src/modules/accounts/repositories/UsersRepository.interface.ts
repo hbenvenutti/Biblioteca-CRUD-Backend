4582c23ed1be921e0485a9b44168a140a2840eb6
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-24 14:56:05 -0300

    fix(accounts): corrigida a tipagem de retorno no repositório de users
    Corrido o retorno do metodo find by email.

--- src/modules/accounts/repositories/UsersRepository.interface.ts
+++ src/modules/accounts/repositories/UsersRepository.interface.ts
@@ -4,7 +4,7 @@ import { User } from '@accounts:entities/User';
 export interface UsersRepositoryInterface {
   create(data: CreateUserDTO): Promise<User>;
   list(): Promise<User[]>;
-  findByEmail(email: string): Promise<User> | undefined;
+  findByEmail(email: string): Promise<User | undefined>;
   findById(id: string): Promise<User[]>;
   delete(id: string): Promise<void>;
 }

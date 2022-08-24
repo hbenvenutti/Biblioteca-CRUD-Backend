8f179f3cf5bc0f85ab8553fcc5f5909a69277221
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-23 21:37:18 -0300

    feat(accounts): adicionada a validação para impedir usuários com e-mails repetidos

--- src/modules/accounts/use-cases/users/create-user/CreateUser.service.ts
+++ src/modules/accounts/use-cases/users/create-user/CreateUser.service.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from 'tsyringe';
 
 import { CreateUserRequestDTO } from '@accounts:dtos/CreateUserRequest.dto';
 import { CreateUserResponse } from '@accounts:dtos/CreateUserResponse.dto';
+import { EmailInUseError } from '@accounts:errors/EmailInUse.error';
+import { PasswordsDontMatchError } from '@accounts:errors/PasswordsDontMatch.error';
 import { UsersRepositoryInterface } from '@accounts:repositories-interfaces/UsersRepository.interface';
 
 // ---------------------------------------------------------------------------------------------- //
@@ -16,19 +18,25 @@ export class CreateUserService {
   async execute(data: CreateUserRequestDTO): Promise<CreateUserResponse> {
     const { name, lastName, email, password, passwordConfirmation } = data;
 
-    //TODO: Verify if password match
+    // ? ---- Compare Passwords --------------------------------------------------------------- ? //
     if (password !== passwordConfirmation) {
-      throw new Error('Method not implemented.');
+      throw new PasswordsDontMatchError();
     }
 
     //TODO: Verify if the e-mail is valid
     //TODO: Verify if data is valid
-    //TODO: Verify if e-mail is not in use
+
+    // ? ---- Verify if user already exists --------------------------------------------------- ? //
+    const emailInUse = await this.usersRepository.findByEmail(email);
+
+    if (emailInUse) {
+      throw new EmailInUseError();
+    }
 
     //TODO: Crypt the password
     const passwordHash = password;
 
-    //? ---- Creates user in db ---------------------------------------------------------------- ?//
+    //? ---- Creates user in db --------------------------------------------------------------- ? //
     const user = await this.usersRepository.create({
       name,
       lastName,

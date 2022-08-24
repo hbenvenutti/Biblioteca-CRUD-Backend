d29168c9e573010f732da2563e3dc69665e9332f
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-24 15:23:45 -0300

    feat(providers): adicionado um retorno negativo ao mock de validação

--- src/shared/containers/providers/validation/Validation.mock.provider.ts
+++ src/shared/containers/providers/validation/Validation.mock.provider.ts
@@ -2,7 +2,7 @@ import { CreateUserRequestDTO } from '@accounts/dtos/CreateUserRequest.dto';
 import { ValidationProviderInterface } from '@shared:containers/providers/validation/Validation.provider.interface';
 
 export class ValidationMockProvider implements ValidationProviderInterface {
-  async validateUserCreationData(_: CreateUserRequestDTO): Promise<boolean> {
-    return true;
+  async validateUserCreationData(data: CreateUserRequestDTO): Promise<boolean> {
+    return data.email !== 'invalid';
   }
 }

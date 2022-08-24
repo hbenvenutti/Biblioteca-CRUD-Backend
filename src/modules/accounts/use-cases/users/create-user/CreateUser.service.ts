759dce3256fb92e54d5036bf4de517d6c97738c8
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-24 16:59:45 -0300

    feat(accounts): implementado o tratamento de strings na criação de usuários

--- src/modules/accounts/use-cases/users/create-user/CreateUser.service.ts
+++ src/modules/accounts/use-cases/users/create-user/CreateUser.service.ts
@@ -2,13 +2,19 @@ import { inject, injectable } from 'tsyringe';
 
 import { CreateUserRequestDTO } from '@accounts:dtos/CreateUserRequest.dto';
 import { CreateUserResponse } from '@accounts:dtos/CreateUserResponse.dto';
+
 import { EmailInUseError } from '@accounts:errors/EmailInUse.error';
 import { InvalidDataError } from '@accounts:errors/InvalidData.error';
 import { PasswordsDontMatchError } from '@accounts:errors/PasswordsDontMatch.error';
+
 import { UserMap } from '@accounts:mappers/User.map';
+
 import { UsersRepositoryInterface } from '@accounts:repositories-interfaces/UsersRepository.interface';
-import { ValidationProviderInterface } from '@shared:containers/providers/validation/Validation.provider.interface';
-import { HashProviderInterface } from '@shared:containers/providers/hash/Hash.provider.interface';
+
+import { ValidationProviderInterface } from '@shared:providers/validation/Validation.provider.interface';
+import { HashProviderInterface } from '@shared:providers/hash/Hash.provider.interface';
+
+import { prepareStringToDatabase } from '@shared/utils/prepareStringToDatabase';
 
 // ---------------------------------------------------------------------------------------------- //
 
@@ -55,11 +61,13 @@ export class CreateUserService {
 
     // *** ---- Creates user in db ---------------------------------------------------------- *** //
     const user = await this.usersRepository.create({
-      name,
-      lastName,
-      email,
+      // ? ---- Prepare String to database removes useless spaces and etc... ---- ? //
+      name: prepareStringToDatabase(name),
+      lastName: prepareStringToDatabase(lastName),
+      email: prepareStringToDatabase(email),
       password: passwordHash
     });
+
 
     // *** ---- Removes password from user -------------------------------------------------- *** //
     return UserMap.toDto(user);

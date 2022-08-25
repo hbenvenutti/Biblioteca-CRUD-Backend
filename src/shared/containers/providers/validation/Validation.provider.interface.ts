f464ec43b6f89f670f72123f4a79f60028570ce6
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-25 19:00:16 -0300

    feat(providers): adicionado metodo para validação de dados de sessão

--- src/shared/containers/providers/validation/Validation.provider.interface.ts
+++ src/shared/containers/providers/validation/Validation.provider.interface.ts
@@ -1,5 +1,7 @@
+import { SessionCreationRequest } from '@accounts:dtos/Sessions.dto';
 import { UserCreationRequest } from '@accounts:dtos/Users.dto';
 
 export interface ValidationProviderInterface {
   validateUserCreationData(data: UserCreationRequest): Promise<boolean>;
+  validateSessionData(data: SessionCreationRequest): Promise<boolean>;
 }

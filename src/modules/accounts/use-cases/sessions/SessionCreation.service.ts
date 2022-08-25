79d7711a8028ed9ed40c24ae05c76c131e9352a6
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-25 19:16:12 -0300

    feat(accounts): implemetadas regras de negocio na criação de sessão
    * Foi implementada a validação de dados da requisição.
    
    * Foi implementada a verificação se o usuário realmente existe.

--- src/modules/accounts/use-cases/sessions/SessionCreation.service.ts
+++ src/modules/accounts/use-cases/sessions/SessionCreation.service.ts
@@ -1,7 +1,14 @@
 import { inject, injectable } from 'tsyringe';
 
 import { SessionCreationRequest, SessionsCreationResponse } from '@accounts:dtos/Sessions.dto';
+
+import { InvalidDataError } from '@accounts:errors/InvalidData.error';
+import { NotFoundError } from '@accounts:errors/NotFound.error';
+
 import { UsersRepositoryInterface } from '@accounts:repositories-interfaces/UsersRepository.interface';
+
+import { HashProviderInterface } from '@shared:providers/hash/Hash.provider.interface';
+import { ValidationProviderInterface } from '@shared:providers/validation/Validation.provider.interface';
 
 // ---------------------------------------------------------------------------------------------- //
 
@@ -9,9 +16,34 @@ import { UsersRepositoryInterface } from '@accounts:repositories-interfaces/User
 class SessionCreationService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: UsersRepositoryInterface
+    private usersRepository: UsersRepositoryInterface,
+
+    @inject('HashProvider')
+    private hashProvider: HashProviderInterface,
+
+    @inject('ValidationProvider')
+    private validationProvider: ValidationProviderInterface
   ) {}
+
   async execute({ email, password }: SessionCreationRequest): Promise<SessionsCreationResponse> {
+    // *** ---- Request data validation ----------------------------------------------------- *** //
+    const emailIsValid = await this.validationProvider.validateSessionData({ email, password });
+
+    if (!emailIsValid) {
+      throw new InvalidDataError();
+    }
+
+    // *** ---- Verify if user exists ------------------------------------------------------- *** //
+    const userExists = await this.usersRepository.findByEmail(email);
+
+    if (!userExists) {
+      throw new NotFoundError();
+    }
+
+    // TODO: Compare passwords
+    // TODO: Generate a session token
+    // TODO: Return user and session token
+    
     throw new Error('Method not implemented.');
   }
 }

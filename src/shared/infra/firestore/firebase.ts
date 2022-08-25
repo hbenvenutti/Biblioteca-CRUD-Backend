4c7334662496b405144b105318cd45ed9d05ac31
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-24 21:49:29 -0300

    fix(firebase): correção na conexão do firebase para testes

--- src/shared/infra/firestore/firebase.ts
+++ src/shared/infra/firestore/firebase.ts
@@ -8,9 +8,15 @@ const serviceAccount: ServiceAccount = {
   privateKey: process.env.PRIVATE_KEY
 };
 
+const testServiceAccount = {
+  projectId: process.env.TEST_PROJECT_ID,
+  clientEmail: process.env.TEST_CLIENT_EMAIL,
+  privateKey: process.env.TEST_PRIVATE_KEY
+};
+
 const credential = process.env.NODE_ENV === 'production'
   ? cert(serviceAccount)
-  : applicationDefault();
+  : cert(testServiceAccount);
 
 const firebaseApp = initializeApp({ credential });
 

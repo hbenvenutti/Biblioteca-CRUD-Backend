e901d72be5b808569e2c8bcf96e3ec807da6ef7e
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-25 01:01:38 -0300

    feat(firestore): implementado um método para buscar a senha salva do usuário
    Esse método serve para testar se a senha foi criptografada ou não.
    Ele só é chamado dentro do escopo de testes.

--- src/shared/infra/database/TestDatabaseFactory.ts
+++ src/shared/infra/database/TestDatabaseFactory.ts
@@ -1,6 +1,9 @@
 import { TestDatabaseInterface } from '@shared/infra/database/TestDatabase.interface';
 import { TestFirestore } from '@firestore/TestFirestore';
 
+/**
+ * Factory that serves as a middleware between tests and actual database.
+ */
 export class TestDatabaseFactory {
   testDatabase: TestDatabaseInterface;
 
--- src/shared/infra/firestore/TestFirestore.ts
+++ src/shared/infra/firestore/TestFirestore.ts
@@ -1,7 +1,12 @@
 import { TestDatabaseInterface } from '@shared/infra/database/TestDatabase.interface';
 import { database }from '@firestore/firestore';
 
-export class TestFirestore implements TestDatabaseInterface {
+// ---------------------------------------------------------------------------------------------- //
+
+/**
+ * Class that implements the methods needed in integration tests.
+ */
+class TestFirestore implements TestDatabaseInterface {
   private database = database;
 
   async deleteAllUsers(): Promise<void> {
@@ -13,4 +18,17 @@ export class TestFirestore implements TestDatabaseInterface {
 
     await batch.commit();
   }
+
+  async getUserPassword(id: string): Promise<string> {
+    const document = await this.database
+      .collection('users')
+      .doc(id)
+      .get();
+
+    const user = document.data();
+
+    return user?.password as string;
+  }
 }
+
+export { TestFirestore };

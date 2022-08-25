d8942d11f2b219410f9e113f87257a9609f0e09e
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-25 20:11:36 -0300

    test(accounts): implementados os testes unitarios de criação de sessão

---
+++ src/modules/accounts/use-cases/sessions/__tests__/SessionCreation.service.spec.ts
@@ -0,0 +1,136 @@
+import 'reflect-metadata';
+
+import { NotFoundError } from '@accounts:errors/NotFound.error';
+
+import { UsersRepositoryMock } from '@accounts:repositories-interfaces/mock/UsersRepository.mock';
+import { UsersRepositoryInterface } from '@accounts:repositories-interfaces/UsersRepository.interface';
+
+import { SessionCreationService } from '@accounts:use-cases/sessions/SessionCreation.service';
+
+import { HashProviderInterface } from '@shared:providers/hash/Hash.provider.interface';
+import { MockHashProvider } from '@shared:providers/hash/Hash.mock.provider';
+
+import { MockValidationProvider } from '@shared:providers/validation/Validation.mock.provider';
+import { ValidationProviderInterface } from '@shared:providers/validation/Validation.provider.interface';
+import { InvalidDataError } from '@accounts:errors/InvalidData.error';
+
+// ---------------------------------------------------------------------------------------------- //
+
+describe('Session Creation Service', () => {
+  let usersRepository: UsersRepositoryInterface;
+  let hashProvider: HashProviderInterface;
+  let validationProvider: ValidationProviderInterface;
+  let sessionCreationService: SessionCreationService;
+
+  const email = 'user@example.com';
+  const password = '@Password248';
+
+  const user = {
+    email,
+    password,
+    passwordConfirmation: password,
+    name: 'john',
+    lastName: 'doe'
+  };
+
+  beforeAll(async () => {
+    usersRepository = new UsersRepositoryMock();
+    hashProvider = new MockHashProvider();
+    validationProvider = new MockValidationProvider();
+
+    sessionCreationService = new SessionCreationService(usersRepository, hashProvider, validationProvider);
+
+    await usersRepository.create(user);
+  });
+
+
+  // *** ---- Success ----------------------------------------------------------------------- *** //
+
+  it('should return a session token', async () => {
+    const sessionResponse = await sessionCreationService.execute({ email, password });
+
+    expect(sessionResponse).toHaveProperty('token');
+    expect(sessionResponse.token).toBeTruthy();
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should return the user\'s data', async () => {
+    const { user } = await sessionCreationService.execute({ email, password });
+
+    expect(user).toHaveProperty('id');
+    expect(user.email).toEqual(email);
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should not return user\'s password', async () => {
+    const { user } = await sessionCreationService.execute({ email, password });
+
+    expect(user).not.toHaveProperty('password');
+  });
+
+
+  // *** ---- Data Validation --------------------------------------------------------------- *** //
+
+  it('should fail if user is not found', async () => {
+    const email = 'wrong@email.com';
+
+    expect(async () => {
+      await sessionCreationService.execute({ email, password });
+    })
+      .rejects
+      .toEqual(new NotFoundError);
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should fail if data is not valid', () => {
+    const email = 'invalid';
+
+    expect(async () => {
+      await sessionCreationService.execute({ email, password });
+    })
+      .rejects
+      .toEqual(new InvalidDataError);
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should fail if password does\'t match', async () => {
+    const password = 'wrong-password';
+
+
+    expect(async () => {
+      await sessionCreationService.execute({ email, password });
+    })
+      .rejects
+      .toEqual(new NotFoundError);
+  });
+  
+
+  // *** ---- Providers  -------------------------------------------------------------------- *** //
+  it('should call validation provider', async () => {
+    const validateSessionData = jest.spyOn(validationProvider, 'validateSessionData');
+
+    await sessionCreationService.execute({ email, password });
+
+    expect(validateSessionData).toHaveBeenCalled();
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should call hash provider', async () => {
+    const compare = jest.spyOn(hashProvider, 'compare');
+
+    await sessionCreationService.execute({ email, password });
+
+    expect(compare).toHaveBeenCalled();
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should call token provider', async () => {
+    expect(true).toBeFalsy();
+  });
+});

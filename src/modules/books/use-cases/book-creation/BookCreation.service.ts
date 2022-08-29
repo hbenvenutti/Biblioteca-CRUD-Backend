cf8a75eb4245050e6be78aae66770e356f878981
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-29 19:46:21 -0300

    feat(books): alterações no service de criação de livros
    Implementada a validação dos dados para a criação dos livros.

--- src/modules/books/use-cases/book-creation/BookCreation.service.ts
+++ src/modules/books/use-cases/book-creation/BookCreation.service.ts
@@ -3,6 +3,8 @@ import { inject, injectable } from 'tsyringe';
 import { BookCreationData } from '@books:dtos/Book';
 import { Book } from '@books:entities/Book';
 import { BooksRepositoryInterface } from '@books:repositories-interfaces/BooksRepository.interface';
+import { ValidationProviderInterface } from '@shared:containers/providers/validation/Validation.provider.interface';
+import { InvalidDataError } from '@errors/InvalidData.error';
 
 // ---------------------------------------------------------------------------------------------- //
 
@@ -10,10 +12,19 @@ import { BooksRepositoryInterface } from '@books:repositories-interfaces/BooksRe
 class BookCreationService {
   constructor(
     @inject('BooksRepository')
-    private booksRepository: BooksRepositoryInterface
+    private booksRepository: BooksRepositoryInterface,
+
+    @inject('ValidationProvider')
+    private validationProvider: ValidationProviderInterface
   ) {}
 
   async execute(data: BookCreationData): Promise<Book> {
+    const isValid = await this.validationProvider.validateBookCreationData(data);
+
+    if(!isValid) {
+      throw new InvalidDataError();
+    }
+
     return this.booksRepository.create(data);
   }
 }

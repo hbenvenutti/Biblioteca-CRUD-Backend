cf25be2c9a365aaee434683d257e61386e2b879a
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-29 18:11:24 -0300

    feat(books): adicionada a rota para criação de livros

--- src/modules/books/infra/http/index.routes.ts
+++ src/modules/books/infra/http/index.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 
+import { BookCreationController } from '@books:use-cases/book-creation/BookCreation.controller';
+
 // ---------------------------------------------------------------------------------------------- //
 
 const booksRouter = Router();
+
+// *** ---- Controllers --------------------------------------------------------------------- *** //
+const bookCreationController = new BookCreationController();
+
+// *** ---- Routes -------------------------------------------------------------------------- *** //
+booksRouter.post('/', bookCreationController.execute );
 
 // ---------------------------------------------------------------------------------------------- //
 

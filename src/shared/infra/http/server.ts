e546d6ee3539e7f50723e09ba37bd6a6fb0a9e5d
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-31 03:55:42 -0300

    refactor(server): alterada a mensagem do servidor para um link http

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,5 @@
 import server from '@shared:app/App';
 
 server.listen((process.env.PORT), () => {
-  console.log(`Listening at port: ${process.env.PORT}\nEnv: ${process.env.NODE_ENV}`);
+  console.log(`Listening at port: http://localhost:${process.env.PORT}\nEnv: ${process.env.NODE_ENV}`);
 });

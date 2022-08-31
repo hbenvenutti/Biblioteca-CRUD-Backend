8b04e55c574b0b52b8cc79f510588cc02d12f301
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-30 23:12:42 -0300

    feat(books): criada uma interface de livro inválido

--- src/modules/books/entities/TestBook.ts
+++ src/modules/books/entities/TestBook.ts
@@ -9,3 +9,11 @@ export class TestBook implements BookCreationData {
   edition = 'edition';
   synopsis = 'synopsis';
 }
+
+export interface InvalidBook {
+  title?: string | boolean;
+  author?: string | boolean;
+  publisher?: string | boolean;
+  edition?: string | boolean;
+  synopsis?: string | boolean;
+}

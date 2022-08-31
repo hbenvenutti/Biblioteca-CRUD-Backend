19272ceff979c15eab99977da9eed4580ca04407
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-31 00:51:22 -0300

    test(books): adicionados testes unitários para a criação de livros

--- src/modules/books/use-cases/book-creation/__tests__/BookCreation.service.spec.ts
+++ src/modules/books/use-cases/book-creation/__tests__/BookCreation.service.spec.ts
@@ -6,15 +6,12 @@ import { BooksRepositoryMock } from '@books:repositories-interfaces/BooksReposit
 import { ValidationProviderInterface } from '@shared:containers/providers/validation/Validation.provider.interface';
 import { MockValidationProvider } from '@shared:containers/providers/validation/Validation.mock.provider';
 import { InvalidDataError } from '@errors/InvalidData.error';
+import { TestBook } from '@books:entities/TestBook';
 
 // ---------------------------------------------------------------------------------------------- //
 
 describe('book creation service', () => {
-  const title = 'test book';
-  const author = 'test author';
-  const publisher = 'test publisher';
-  const edition = 'test edition';
-  const synopsis = 'test synopsis';
+  let bookData: TestBook;
 
   let booksRepository: BooksRepositoryInterface;
   let validationProvider: ValidationProviderInterface;
@@ -26,20 +23,26 @@ describe('book creation service', () => {
     bookCreationService = new BookCreationService(booksRepository, validationProvider);
   });
 
+  beforeEach(async () => {
+    bookData = new TestBook();
+  });
+
   // -------------------------------------------------------------------------------------------- //
 
   it('should create a book and generate a id', async () => {
-    const user = await bookCreationService.execute({ title, author, publisher, edition, synopsis });
+    const book = await bookCreationService.execute(bookData);
 
-    expect(user).toHaveProperty('id');
+    expect(book).toHaveProperty('id');
   });
 
   // -------------------------------------------------------------------------------------------- //
 
   it('should fail if data is invalid', () => {
+    bookData.title = 'invalid';
+
     expect(async () => {
       await bookCreationService
-        .execute({ title: 'invalid', author, publisher, edition, synopsis });
+        .execute(bookData);
     })
       .rejects
       .toEqual(new InvalidDataError());
@@ -50,8 +53,88 @@ describe('book creation service', () => {
   it('should call the validation provider', async () => {
     const validateBookCreationData = jest.spyOn(validationProvider, 'validateBookCreationData');
 
-    await bookCreationService.execute({ title, author, publisher, edition, synopsis });
+    await bookCreationService.execute(bookData);
 
     expect(validateBookCreationData).toHaveBeenCalled();
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should turn title into lowercase', async () => {
+    bookData.title = 'TITLE';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.title).toEqual('title');
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should turn author into lowercase', async () => {
+    bookData.author = 'AUTHOR';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.author).toEqual('author');
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should turn publisher into lowercase', async () => {
+    bookData.publisher = 'PUBLISHER';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.publisher).toEqual('publisher');
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should turn edition into lowercase', async () => {
+    bookData.edition = 'EDITION';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.edition).toEqual('edition');
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should remove useless spaces from title', async () => {
+    bookData.title = ' title ';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.title).toEqual('title');
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should remove useless spaces from author', async () => {
+    bookData.author = ' author ';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.author).toEqual('author');
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should remove useless spaces from publisher', async () => {
+    bookData.publisher = ' publisher ';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.publisher).toEqual('publisher');
+  });
+
+  // -------------------------------------------------------------------------------------------- //
+
+  it('should remove useless spaces from edition', async () => {
+    bookData.edition = ' edition ';
+
+    const book = await bookCreationService.execute(bookData);
+
+    expect(book.edition).toEqual('edition');
   });
 });

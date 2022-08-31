8279d8ceba3938f6fcd3a658ccecba8dd94a7da7
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-31 00:54:51 -0300

    feat(books): adicionado o tratamento das strings na criação de livros
    Implementada a remoção de espaços e transformação em caixa baixa.

--- src/modules/books/use-cases/book-creation/BookCreation.service.ts
+++ src/modules/books/use-cases/book-creation/BookCreation.service.ts
@@ -5,6 +5,7 @@ import { Book } from '@books:entities/Book';
 import { BooksRepositoryInterface } from '@books:repositories-interfaces/BooksRepository.interface';
 import { ValidationProviderInterface } from '@shared:containers/providers/validation/Validation.provider.interface';
 import { InvalidDataError } from '@errors/InvalidData.error';
+import { prepareStringToDatabase } from '@shared:utils/prepareStringToDatabase';
 
 // ---------------------------------------------------------------------------------------------- //
 
@@ -24,6 +25,11 @@ class BookCreationService {
     if(!isValid) {
       throw new InvalidDataError();
     }
+
+    data.title = prepareStringToDatabase(data.title);
+    data.author = prepareStringToDatabase(data.author);
+    data.publisher = prepareStringToDatabase(data.publisher);
+    data.edition = prepareStringToDatabase(data.edition);
 
     return this.booksRepository.create(data);
   }

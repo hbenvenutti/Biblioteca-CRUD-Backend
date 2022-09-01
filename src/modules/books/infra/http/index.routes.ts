5f3d251f3a654be5af5296f6f48705d41120d9f7
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-09-01 00:17:37 -0300

    feat(books): criada a rota delete para books

--- src/modules/books/infra/http/index.routes.ts
+++ src/modules/books/infra/http/index.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
-import { BookCreationController } from '@books:use-cases/book-creation/BookCreation.controller';
 import { authentication } from '@middlewares/authentication';
+import { BookCreationController } from '@books:use-cases/book-creation/BookCreation.controller';
+import { BookDeletionController } from '@books:use-cases/book-deletion/BookDeletion.controller';
 
 // ---------------------------------------------------------------------------------------------- //
 
@@ -9,12 +10,14 @@ const booksRouter = Router();
 
 // *** ---- Controllers --------------------------------------------------------------------- *** //
 const bookCreationController = new BookCreationController();
+const bookDeletionController = new BookDeletionController();
 
 // *** ---- Routes -------------------------------------------------------------------------- *** //
 
 // *** ---- Authorized ---------------------------------------------------------------------- *** //
 booksRouter.use(authentication);
 booksRouter.post('/', bookCreationController.execute );
+booksRouter.delete('/:id', bookDeletionController.execute);
 
 // ---------------------------------------------------------------------------------------------- //
 

3d75bfe2ee0b0e1ddae5579def084e9a731674a9
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-09-01 00:55:41 -0300

    feat(database): adicionado o método seed book à interface de banco de dados

--- src/shared/infra/database/TestDatabase.interface.ts
+++ src/shared/infra/database/TestDatabase.interface.ts
@@ -1,3 +1,8 @@
+import { User } from '@accounts:entities/User';
+import { Book } from '@books:entities/Book';
+
+// ---------------------------------------------------------------------------------------------- //
+
 /**
  * Interface that serves as a model to implement database methods for tests.
  *
@@ -8,7 +13,8 @@
 export interface TestDatabaseInterface {
   deleteAllUsers(): Promise<void>;
   getUserPassword(id: string): Promise<string>
-  seedUser(): Promise<void>;
+  seedUser(): Promise<User>;
+  seedBook(): Promise<Book>
 
   deleteAllBooks(): Promise<void>;
 }
